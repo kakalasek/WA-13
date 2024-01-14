79ad26aa1c31bf5891cba92c24988f20118aa9ea
kakalasek <honzicekkj@seznam.cz>
2024-01-14 21:50:30 +0100

    form structure

--- script_clas.js
+++ script_clas.js
@@ -1,3 +1,4 @@
+/* Classes */
 class Author {
   constructor(name, surname, date_of_birth) {
     this.name = name;
@@ -23,5 +24,44 @@ class Gallery {
   addPainting(painting) {
     if (painting !== Painting) throw "Not a painting";
     this.paintings.push(painting);
+    this.paintings.sort((p1, p2) => {
+      p2.price - p1.price;
+    });
+  }
+
+  finalPrice() {
+    let output = 0;
+    this.paintings.forEach((element) => {
+      output += element.price;
+    });
+    return output;
+  }
+
+  expensiveThree() {
+    let output = [];
+    for (let i = 0; i < 3; i++) {
+      if (this.paintings[i] === undefined) break;
+      output.push([this.paintings[i].author, this.paintings[i].price]);
+    }
+    return output;
+  }
+
+  allPaintingsFrom(author) {
+    let output = [];
+    if (author !== Author) throw "Not an author";
+    output[0] = author;
+    output[1] = [];
+    this.paintings.forEach((element) => {
+      if (element.author === author) {
+        output[1].push([element.name, element.price]);
+      }
+    });
+    return output;
+  }
+
+  allPaintings() {
+    let output = [];
   }
 }
+
+/* Form stuff */
